6b7a8094adfab51239174db691aedff7349c67bf
Bhavin Trivedi <dearbhavin@hotmail.com>
2021-07-29 19:34:53 +0530

    Document and Window Object example

---
+++ ch_001/documentObject.js
@@ -0,0 +1,11 @@
+console.log('document.body')
+console.log(document.body);
+console.log('document.title');
+console.log(document.title);
+console.log('document.URL');
+console.log(document.URL);
+document.body.style.backgroundColor = 'yellow';
+document.getElementById('content').innerText = "We will do it";
+console.log(window);
+console.log(window.location);
+console.log(window.history);
