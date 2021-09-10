a2ad9db52a2fb7e1272034e322e7b484c5dbf613
Bhavin Trivedi <dearbhavin@hotmail.com>
2021-09-10 20:48:46 +0530

    Example of click event

---
+++ ch_001/eventdemo.js
@@ -0,0 +1,8 @@
+// Function without any argument
+function sayHello(){
+    alert('Hello from external JS');
+}
+// Function contains argument name message 
+function sayMessage(message){
+    alert(message);
+}
