c660d570e1ffb1a1f49dfd343fcb2dbfb3dd2227
Bhavin Trivedi <dearbhavin@hotmail.com>
2021-09-13 16:20:58 +0530

    Event Example added

---
+++ ch_003_various_event/eventExample.js
@@ -0,0 +1,16 @@
+function buttonOnClick(){
+    alert('Button onclick');
+}
+function buttononmouseover(){
+    alert('Button buttononmouseover');
+}
+function buttononmouseout(){
+    alert('Button buttononmouseout');
+}
+function logonkeydownevent(event){
+console.log('key down');
+console.log(event);
+console.log(event.key);
+console.log(event.keyCode);
+console.log(event.target);
+}
